fc14b06a33790c94000dceb7faf76cf1cd735322
maxefimov <efimov.academy.top@gmail.com>
2023-03-15 21:27:04 +0300

    Добавьте файлы проекта.

---
+++ ExampleClassLibrary/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleClassLibrary
+{
+    public class Address
+    {
+        public string? City { set; get; }
+        public string? Street { set; get; }
+        public Address() { }
+    }
+}
---
+++ ExampleClassLibrary/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExampleClassLibrary
+{
+    public class User
+    {
+        public string? Name { set; get; }
+
+        //[JsonIgnore]
+        //[JsonPropertyName("UserAge")]
+        public int Age { set; get; }
+        public Address? Address { set; get; }
+        public User() { }
+
+        public User(string? name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public User(string? name, int age, Address address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+    }
+}
